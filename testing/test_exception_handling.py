adb22e64927a468b577ea01101845c6b91c8bcd4
Simon Walker <s.r.walker101@googlemail.com>
2014-08-13 12:03:15 +0100

    Mocking set_wcs_status for exception handling test
    This is not under test, and crashed when trying to
    open a MagicMock

--- testing/test_exception_handling.py
+++ testing/test_exception_handling.py
@@ -5,7 +5,8 @@ sys.path.extend(['.', 'testdata'])
 from NGTS_workpackage.wcs_fitting import handle_errors_in_casu_solve
 
 @mock.patch('NGTS_workpackage.wcs_fitting.casu_solve')
-def test_exception_handling(mock_casu_solve):
+@mock.patch('NGTS_workpackage.wcs_fitting.set_wcs_status')
+def test_exception_handling(mock_set_wcs_status, mock_casu_solve):
     casuin = mock.MagicMock()
     wcsref = mock.MagicMock()
     mock_casu_solve.side_effect = RuntimeError("Test exception")
