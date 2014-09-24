29f683082247609c7d58e5f40b43d1fd5046549e
Simon Walker <s.r.walker101@googlemail.com>
2014-09-24 12:08:30 +0100

    Add usage error on incorrect invocation

--- NGTS_workpackage/dist_solve.sh
+++ NGTS_workpackage/dist_solve.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+if [[ "$#" -ne 4 ]]; then
+    echo "Usage: $0 <casuin> <catname> <chainname> <catsrc>" >&2
+    echo "See documentation: http://ngts.warwick.ac.uk/twiki/bin/view/Main/ZLPDocAstrometrySolver" >&2
+    exit 1
+fi
+
 CASUIN=$1
 MYCATNAME=$2
 CHAIN_NAME=$3
