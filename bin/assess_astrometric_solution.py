cdd18448d0f2def5d8a5fd776d342807dfb1b3f9
Simon Walker <s.r.walker101@googlemail.com>
2014-10-03 10:58:38 +0100

    Add --reduced flag
    Assume the frame is not reduced and remove
    overscan strips unless flag is given

--- bin/assess_astrometric_solution.py
+++ bin/assess_astrometric_solution.py
@@ -5,10 +5,40 @@ import argparse
 from NGTS_workpackage.wcs_fitting import (initialise_wcs_cache,
                                           casu_solve,
                                           )
+import fitsio
+import os
 try:
     import cPickle as pickle
 except ImportError:
     import pickle
+
+
+def image_size(fname):
+    '''
+    Return the image size from the Primary HDU
+    '''
+    return fitsio.read(fname).shape
+
+
+def remove_overscan_strips(fname):
+    '''
+    Given an image of dimensions 2048x2088 remove the two overscan strips so
+    the solution will work on solved frames
+    '''
+    if not image_size(fname) == (2048, 2088):
+        raise RuntimeError("Frame has dimensions {}, this is likely not a raw "
+                           "NGTS frame".format(image_size(fname)))
+
+    with fitsio.FITS(fname) as infile:
+        image_data = infile[0].read()
+        header = infile[0].read_header()
+
+    without_overscan = image_data[:, 20:-20]
+    out_fname = os.path.splitext(fname)[0] + '_without_overscan_strips.fits'
+    with fitsio.FITS(out_fname, 'rw', clobber=True) as outfile:
+        outfile.write(without_overscan, header=header)
+
+    return out_fname
 
 
 def main(args):
@@ -16,9 +46,22 @@ def main(args):
         dist_map = pickle.load(infile)
 
     catpath = 'catcache'
-    initialise_wcs_cache(args.filename, catpath, args.wcsref,
+
+    if not args.reduced:
+        fname = remove_overscan_strips(args.filename)
+    else:
+        fname = args.filename
+
+    print('Frame {} will be altered'.format(fname))
+
+    assert image_size(fname) == (2048, 2048), (
+        "Image incorrect shape, "
+        "should be 2048x2048, is {}".format(image_size(fname))
+    )
+
+    initialise_wcs_cache(fname, catpath, args.wcsref,
                          thresh=20.0, verbose=False)
-    casu_solve(args.filename, args.wcsref, dist_map=dist_map, catpath=catpath)
+    casu_solve(fname, args.wcsref, dist_map=dist_map, catpath=catpath)
 
 
 if __name__ == '__main__':
@@ -26,4 +69,6 @@ if __name__ == '__main__':
     parser.add_argument('filename')
     parser.add_argument('-s', '--solution', required=True)
     parser.add_argument('-w', '--wcsref', required=True)
+    parser.add_argument('--reduced', required=False, action='store_true',
+                        default=False, help='Is the frame reduced?')
     main(parser.parse_args())
