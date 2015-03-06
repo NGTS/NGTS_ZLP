b7d659c4ed238beedb3ac5b38695863b0b3edc25
Simon Walker <s.r.walker101@googlemail.com>
2015-03-06 15:01:06 +0000

    Importing subpackages properly

--- NGTS_workpackage/__init__.py
+++ NGTS_workpackage/__init__.py
@@ -1,6 +1,6 @@
-from wcs_fitting import *
-from wcs_photom import *
-from condense_data import *
-from util import *
-from quality_checks import *
+from .wcs_fitting import *
+from .wcs_photom import *
+from .condense_data import *
+from .util import *
+from .quality_checks import *
 import casutools
