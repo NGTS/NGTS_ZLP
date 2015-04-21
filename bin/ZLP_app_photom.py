09f958ada189db27ba200dfdcf91202566de622f
Simon Walker <s.r.walker101@googlemail.com>
2015-04-21 16:12:35 +0100

    Remove commented out dist

--- bin/ZLP_app_photom.py
+++ bin/ZLP_app_photom.py
@@ -41,10 +41,6 @@ def main(argv):
     outfile = open(argv.outlist, 'w')
     outfile.close()
 
-    #dist_map = {'CD2_1': -1.4191656988099457e-08, 'CD2_2': 0.0013885222778780631, 'RA_s': 0.055870512902160017, 'CD1_2': -1.8513409749920829e-07, 'CD1_1': 0.0013883723486931344, 'CRVAL2': 49.414386141920922, 'CRPIX1': 1002.0594111585548, 'CRPIX2': 982.11907123259516, 'CRVAL1': 285.95176591229813, 'PV2_1': 1.0, 'PV2_2': 0.0, 'PV2_3': 7.898207770193908, 'PV2_5': 1392.9968242630193, 'PV2_7': -265152.6479961705, 'DEC_s': 0.22189846295346452, 'CTYPE2': 'DEC--ZPN', 'CTYPE1': 'RA---ZPN'}
-
-    #test_map = '/ngts/pipedev/AperturePhot/test_map.p'
-
     dist_map = pickle.load(open(argv.dist, 'r'))
 
     filelist = argv.filelist
