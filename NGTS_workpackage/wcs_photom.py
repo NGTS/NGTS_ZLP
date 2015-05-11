f14e14331d2a9bd114834fedc0395c82007564e8
Simon Walker <s.r.walker101@googlemail.com>
2015-05-11 17:13:27 +0100

    Printing imcore images before solving

--- NGTS_workpackage/wcs_photom.py
+++ NGTS_workpackage/wcs_photom.py
@@ -84,6 +84,7 @@ def wcs_photom(image,
 
     outname = image + '.phot'
 
+    print('IMCORE: {}'.format(image))
     casutools.imcore_list(image, cat_file, outname,
                           confidence_map=conf_file,
                           rcore=appsize,
